f495af7b56d7de84314ff9070d12290d9f30f0c9
EasyStarPilot <EasyStarPilot@gmx.de>
2024-11-03 22:07:09 +0100

    2 Colors

--- TTT.java
+++ TTT.java
@@ -4,109 +4,87 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.text.NumberFormat;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
-
 class TTTAppWindow extends JDialog {
 
-	JButton[] buttons  = new JButton[9];
-    JTextField 	textfield_output;
+	JButton[] buttons = new JButton[9];
+	JTextField textfield_output;
+	boolean player = false;
 
 	public TTTAppWindow() {
 		this.getContentPane().setLayout(null);
-
 		this.initWindow();
-
 		this.addWindowListener(new WindowListener() {
-
 			public void windowClosed(WindowEvent arg0) {
-
-
 			}
-
 			public void windowActivated(WindowEvent e) {
-
-
 			}
-
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-
 			public void windowDeactivated(WindowEvent e) {
-
-
 			}
-
 			public void windowDeiconified(WindowEvent e) {
-
-
 			}
-
 			public void windowIconified(WindowEvent e) {
-
-
 			}
-
 			public void windowOpened(WindowEvent e) {
-
-
 			}
-
-
-
 		});
 
 	}
 
-	protected void initWindow() 
-	{	
+	protected void initWindow() {
 		textfield_output = new JTextField();
 		textfield_output.setEditable(false);
-		for (int i =0; i<9; i++) {
-            buttons[i] = new JButton("" + i); 
-            buttons[i].setBounds(10+i%3*60,i/3*60+10,50,50); 
-            this.getContentPane().add(buttons[i]);
+		for (int i = 0; i < 9; i++) {
+			buttons[i] = new JButton();
+			buttons[i].setBounds(10 + i % 3 * 60, i / 3 * 60 + 10, 50, 50);
+			this.getContentPane().add(buttons[i]);
+			buttons[i].setBackground(Color.WHITE);
 			buttons[i].addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
-					((JButton)arg0.getSource()).setBackground(Color.YELLOW);
+					JButton button = ((JButton) arg0.getSource());
+					if (button.getBackground() == Color.WHITE) {
+						if (player == true) {
+							button.setBackground(Color.YELLOW);
+							player = false;
+						} else {
+							button.setBackground(Color.RED);
+							player = true;
+						}
+						textfield_output.setText("");
+						// if (Win(buttons) == true) {
+
+						// }
+					} else {
+						textfield_output.setText("Das darfst du nicht!!!");
+					}
 				}
 			});
-        } 
-
+		}
 
 		// Positionen festlegen
-		textfield_output.setBounds(10,190,170,25);
-
+		textfield_output.setBounds(10, 190, 170, 25);
 
 		// Elemente dem Fenster hinzufügen:
 		this.getContentPane().add(textfield_output);
-		
-
-		ActionListener escListener = new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		};
-	
-		this.getRootPane().registerKeyboardAction(escListener
-		,
-				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
-				JComponent.WHEN_IN_FOCUSED_WINDOW);
 		this.pack();
 	}
 }
 
+public static boolean Win() {
+
+}
+
 public class TTT {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		TTTAppWindow theAppWindow = new TTTAppWindow();
 		theAppWindow.setBounds(105, 133, 210, 265);
 		theAppWindow.setLocationRelativeTo(null);
