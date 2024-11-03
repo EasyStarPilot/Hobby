452044f8770168fdb4c62861bf9f3001bf1ced13
EasyStarPilot <EasyStarPilot@gmx.de>
2024-11-03 13:51:30 +0100

    copy out of out

--- Make_exe.bat
+++ Make_exe.bat
@@ -1,3 +1,4 @@
 rd /s /q out
 java -jar c:\Users\mail\Downloads\packr-all-4.0.0.jar --executable Taschenrechner --platform windows64 --jdk "C:\Program Files\Eclipse Adoptium\jdk-21.0.4.7-hotspot" --mainclass GUIRechner --output out --classpath .\Hobby.jar
+xcopy /y out\Taschenrechner.exe .
 pause
